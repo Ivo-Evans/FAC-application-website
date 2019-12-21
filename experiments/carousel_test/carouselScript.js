5159695b57e9c0aee791ebaac92f4e1273ea9a03
Ivo Evans Storrie <ievans.storrie@gmail.com>
2019-12-21 23:55:36 +0000

    justify constant: space around. Some lets now consts. Images have green borders for debugging

--- experiments/carousel_test/carouselScript.js
+++ experiments/carousel_test/carouselScript.js
@@ -1,25 +1,25 @@
-let imageStrip = document.body.querySelector(".images");
-let images = document.body.querySelectorAll(".images img")
-let finalImagePosition = Array.from(images).reduce((a, b) => a + b.offsetWidth, 0) - images[images.length - 1].offsetWidth;
-let rightButton = document.getElementById("rButton");
-let leftButton = document.getElementById("lButton");
+const imageStrip = document.body.querySelector(".images");
+const images = document.body.querySelectorAll(".images img")
+const finalImagePosition = Array.from(images).reduce((a, b) => a + b.offsetWidth, 0) - images[images.length - 1].offsetWidth;
+const rightButton = document.getElementById("rButton");
+const leftButton = document.getElementById("lButton");
 
 let currentImage = 0;
 let currentPosition = 0;
 
-padNarrowImages()
+padNarrowImages();
 rightButton.addEventListener('click', () => {moveRight(imageStrip)});
 leftButton.addEventListener('click', () => {moveLeft(imageStrip)});
 
+let boxWidth = document.body.querySelector(".slider_box");
 
 function padNarrowImages() {
-  document.body.style.backgroundColor = "yellow";
-  let widest_image = Array.from(images).map(e => e.offsetWidth).sort((a, b) =>  b - a)[0]; // verified
+  let widest_image = Array.from(images).map(e => e.offsetWidth).sort((a, b) =>  b - a)[0];
   for(let i = 0; i < images.length; i++) {
-    thisWidth = images[i].offsetWidth
-    if (thisWidth < widest_image) { // actually does get triggered for 4/6 images
+    thisWidth = images[i].offsetWidth;
+    if (thisWidth < widest_image) {
       let extra = (widest_image - thisWidth) / 2;
-      images[i].style.paddingLeft = extra + "px"; // the problem must be here
+      images[i].style.paddingLeft = extra + "px";
       images[i].style.paddingRight = extra + "px";
     }
   }
@@ -38,7 +38,7 @@ function moveLeft(element) {
 }
 
 function moveRight(element) {
- currentPosition -= images[currentImage].offsetWidth;
+  currentPosition -= images[currentImage].offsetWidth;
   element.style.transform = "translateX(" +currentPosition + "px)";
   currentImage += 1; // decrementing syntax caused hoisting problems
 
