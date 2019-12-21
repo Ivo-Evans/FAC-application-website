ebaa078af6d6bf62955d6bcf03d8a5e8ef45d01c
Ivo Evans Storrie <ievans.storrie@gmail.com>
2019-12-21 18:40:42 +0000

    add button with event listener that moves image carousel right 500px on click

--- experiments/carousel_test/carouselScript.js
+++ experiments/carousel_test/carouselScript.js
@@ -1,8 +1,11 @@
 let imageStrip = document.body.querySelector(".images");
-let inchoateButton = document.getElementById("rBody");
+let inchoateButton = document.getElementById("rButton");
 
 function move(element, distance, leftOrRight) {
+  console.log("move called")
   element.style.transform = "translateX(" + leftOrRight + distance + "px)";
 }
+
+inchoateButton.addEventListener('click', () => {move(imageStrip, 500, "+")});
 
 move(imageStrip, 500, "-");
