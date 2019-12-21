37cab442e0ace381c82e0c10f5cc3826d28c0e09
Ivo Evans Storrie <ievans.storrie@gmail.com>
2019-12-21 19:25:07 +0000

    delete move, add moveRight and moveLeft. Methods now move slider by shifting slider by offsetWidth of current image - position is saved to a global position variable

--- experiments/carousel_test/carouselScript.js
+++ experiments/carousel_test/carouselScript.js
@@ -1,11 +1,41 @@
 let imageStrip = document.body.querySelector(".images");
+let images = document.body.querySelectorAll(".images img")
 let rightButton = document.getElementById("rButton");
 let leftButton = document.getElementById("lButton");
+let currentImage = 0;
+let position = 0;
 
-function move(element, distance, leftOrRight) {
-  console.log("move called")
-  element.style.transform = "translateX(" + leftOrRight + distance + "px)";
+function moveRight(element) {
+  position -= images[currentImage].offsetWidth;
+  element.style.transform = "translateX(+" + position + "px)";
+  currentImage -= 1; // decrementing syntax caused hoisting problems
 }
 
-rightButton.addEventListener('click', () => {move(imageStrip, 500, "+")});
-leftButton.addEventListener('click', () => {move(imageStrip, 500, "-")});
+function moveLeft(element) {
+  position += images[currentImage].offsetWidth;
+  element.style.transform = "translateX(-" + position + "px)";
+  currentImage += 1; // decrementing syntax caused hoisting problems
+}
+
+
+
+rightButton.addEventListener('click', () => {moveRight(imageStrip)});
+leftButton.addEventListener('click', () => {moveLeft(imageStrip)});
+
+
+
+
+// rightButton.addEventListener('click', () => {move(imageStrip, "+")});
+// leftButton.addEventListener('click', () => {move(imageStrip, "-")});
+
+
+
+
+// function move(element, leftOrRight) {
+//   let newPosition = images[currentImage].offsetWidth + distanceLog;
+//   console.log(distanceToMove)
+//   element.style.transform = "translateX(" + leftOrRight + distanceToMove + "px)";
+//   leftOrRight == "-" ? currentImage++ : currentImage--; // this seems paradoxical but is actually correct
+//   leftOrRight == "-" ? distanceLog += distanceToMove : distanceLog -= distanceToMove;
+//   console.log(currentImage);
+// }
