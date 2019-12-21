6707fac8c8edb6eeb687fce209c0fea60bf0035d
Ivo Evans Storrie <ievans.storrie@gmail.com>
2019-12-21 19:28:20 +0000

    switch moveLeft and moveRight direction of effect - no inversion

--- experiments/carousel_test/carouselScript.js
+++ experiments/carousel_test/carouselScript.js
@@ -5,13 +5,13 @@ let leftButton = document.getElementById("lButton");
 let currentImage = 0;
 let position = 0;
 
-function moveRight(element) {
+function moveLeft(element) {
   position -= images[currentImage].offsetWidth;
   element.style.transform = "translateX(+" + position + "px)";
   currentImage -= 1; // decrementing syntax caused hoisting problems
 }
 
-function moveLeft(element) {
+function moveRight(element) {
   position += images[currentImage].offsetWidth;
   element.style.transform = "translateX(-" + position + "px)";
   currentImage += 1; // decrementing syntax caused hoisting problems
@@ -21,21 +21,3 @@ function moveLeft(element) {
 
 rightButton.addEventListener('click', () => {moveRight(imageStrip)});
 leftButton.addEventListener('click', () => {moveLeft(imageStrip)});
-
-
-
-
-// rightButton.addEventListener('click', () => {move(imageStrip, "+")});
-// leftButton.addEventListener('click', () => {move(imageStrip, "-")});
-
-
-
-
-// function move(element, leftOrRight) {
-//   let newPosition = images[currentImage].offsetWidth + distanceLog;
-//   console.log(distanceToMove)
-//   element.style.transform = "translateX(" + leftOrRight + distanceToMove + "px)";
-//   leftOrRight == "-" ? currentImage++ : currentImage--; // this seems paradoxical but is actually correct
-//   leftOrRight == "-" ? distanceLog += distanceToMove : distanceLog -= distanceToMove;
-//   console.log(currentImage);
-// }
