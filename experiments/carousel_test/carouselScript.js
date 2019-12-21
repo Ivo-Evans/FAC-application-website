b7da38eb7ddbb41dfc06e585fdd3e3d0897eca4a
Ivo Evans Storrie <ievans.storrie@gmail.com>
2019-12-21 22:30:06 +0000

    remove typo

--- experiments/carousel_test/carouselScript.js
+++ experiments/carousel_test/carouselScript.js
@@ -6,26 +6,26 @@ let rightButton = document.getElementById("rButton");
 let leftButton = document.getElementById("lButton");
 
 let currentImage = 0;
-let cuurentPosition = 0;
+let currentPosition = 0;
 
 rightButton.addEventListener('click', () => {moveRight(imageStrip)});
 leftButton.addEventListener('click', () => {moveLeft(imageStrip)});
 
-/*
+
 padNarrowImages(images) // unsuccessful, but changing .images img in the CSS IS successful, so i think the problem is with my interaction with the DOM.
 
 function padNarrowImages(list) {
-  let widest_image = Array.from(list).map(e => e.offsetWidth).sort((a, b) =>  b - a)[0];
+  let widest_image = Array.from(list).map(e => e.offsetWidth).sort((a, b) =>  b - a)[0]; // verified
   for(let i = 0; i < list.length; i++) {
     thisWidth = list[i].offsetWidth
-    if (thisWidth < widest_image) {
+    if (thisWidth < widest_image) { // actually does get triggered for 4/6 images
       let extra = (widest_image - thisWidth) / 2;
-      list[i].style.paddingLeft = extra;
+      list[i].style.paddingLeft = extra; // the problem must be here
       list[i].style.paddingRight = extra;
     }
   }
 }
-*/
+
 
 function moveLeft(element) {
   currentPosition += images[currentImage].offsetWidth;
