fe2e5f7c369d4504057ae8dcec006b5ee19ddc04
Ivo Evans Storrie <ievans.storrie@gmail.com>
2019-12-21 22:55:27 +0000

    add and call padNarrowImages function to space img elements equally

--- experiments/carousel_test/carouselScript.js
+++ experiments/carousel_test/carouselScript.js
@@ -1,31 +1,33 @@
 let imageStrip = document.body.querySelector(".images");
 let images = document.body.querySelectorAll(".images img")
 let finalImagePosition = Array.from(images).reduce((a, b) => a + b.offsetWidth, 0) - images[images.length - 1].offsetWidth;
-
 let rightButton = document.getElementById("rButton");
 let leftButton = document.getElementById("lButton");
 
 let currentImage = 0;
 let currentPosition = 0;
 
+padNarrowImages()
 rightButton.addEventListener('click', () => {moveRight(imageStrip)});
 leftButton.addEventListener('click', () => {moveLeft(imageStrip)});
 
 
-padNarrowImages(images) // unsuccessful, but changing .images img in the CSS IS successful, so i think the problem is with my interaction with the DOM.
-
-function padNarrowImages(list) {
-  let widest_image = Array.from(list).map(e => e.offsetWidth).sort((a, b) =>  b - a)[0]; // verified
-  for(let i = 0; i < list.length; i++) {
-    thisWidth = list[i].offsetWidth
+function padNarrowImages() {
+  document.body.style.backgroundColor = "yellow";
+  let widest_image = Array.from(images).map(e => e.offsetWidth).sort((a, b) =>  b - a)[0]; // verified
+  for(let i = 0; i < images.length; i++) {
+    thisWidth = images[i].offsetWidth
     if (thisWidth < widest_image) { // actually does get triggered for 4/6 images
       let extra = (widest_image - thisWidth) / 2;
-      list[i].style.paddingLeft = extra; // the problem must be here
-      list[i].style.paddingRight = extra;
+      console.log("in conditional")
+      console.log(images[i].style.paddingLeft)
+      images[i].style.paddingLeft = extra + "px"; // the problem must be here
+      console.log(images[i].style.paddingLeft)
+      images[i].style.paddingRight = extra + "px";
+      document.body.style.backgroundColor = "pink";
     }
   }
 }
-
 
 function moveLeft(element) {
   currentPosition += images[currentImage].offsetWidth;
