e045e2a48a7b53ad5dfe6aefff007ddef3e0add7
Ivo Evans Storrie <ievans.storrie@gmail.com>
2019-12-30 22:56:20 +0000

    add smooth transform to move function

--- newScripts.js
+++ newScripts.js
@@ -4,9 +4,10 @@ let jumpWidth = imageStrip.offsetWidth; // this won't be variable
 let pixelPosition = 0;
 
 function move(n) {
+  imageStrip.style.transition = "transform 0.5s ease-in-out";
   jumpWidth = document.querySelector(".carousel_images").offsetWidth;
   pixelPosition += (n * jumpWidth);
-  imageStrip.style.left = pixelPosition + "px";
+  imageStrip.style.transform = "translateX(" + pixelPosition + "px)"
 }
 
-// currently, resize really messes things up. I see two problems (there may be more). Firstly, the image on the screen goes to the wrong place. Secondly,
+// currently, resize really messes things up. I see two problems (there may be more). Firstly, the image on the screen goes to the wrong place instantly on resize. Secondly, I think, image distance needs to be recalibrated. One thing you could do is assign all your variables on load, and assign them all on resize.
